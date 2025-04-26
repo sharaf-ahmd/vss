d7cd152218e1ce204b97b36c2ebcac1d648b2710
Sharaf <sharaf.ams.11@gmail.com>
2025-04-26 15:30:56 +0530

    final commmit

--- backend/models/stripe.js
+++ backend/models/stripe.js
@@ -3,6 +3,6 @@ import Stripe from 'stripe';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-const stripe = new Stripe('sk_test_51RGhSzFNC9lumuodSM9OScNcvhnCAEVf6SL4Ysi9xq04dhnrPFqkt72YnhVSOUKwJne2N2mQstxdbW0sdopQ66xM00Plw8XaqC');
+const stripe = new Stripe
 
 export default stripe;
